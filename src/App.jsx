ef8f5ae26ab41834cd0a0732236e3d655fb2ceca
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2022-07-30 12:23:18 +0800

    Create: Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavigationBar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/global.css';
+// import './assets/media.css';
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,22 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const NavigationBar = () => {
   return (
     <>
-    <Navbar bg="light" expand="lg" fixed="top">
-      <Container>
-        <Navbar.Brand href="#home">DeineLieblingsJasmin</Navbar.Brand>
+    <Navbar bg="light" expand="lg" fixed="top" className="navbar nav-link">
+      <Container className="navbar-nav">
+        <Navbar.Brand href="#home" className="navbar-brand">DeineLieblingsJasmin</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link class="nav-link nav-link" href="#about-me">About Me</Nav.Link>
+            <Nav.Link href="#link" className="nav-link">Hiking</Nav.Link>
+            <Nav.Link href="#link" className="nav-link">Others</Nav.Link>
+            <Nav.Link href="#link" className="nav-link">Contact</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+              <NavDropdown.Item href="#action/3.1" className="dropdown-item">
+                Placeholder
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="#action/3.4" className="dropdown-item">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
@@ -29,30 +29,6 @@ const NavigationBar = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
-    {/* <Navbar bg="dark" expand="lg" fixed="top">
-      <Container>
-        <Navbar.Brand href="#home" id="nav-upperleft">Anything is possible with action, consistency and perseverance.</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" aria-current="page" className="nav-link">Home</Nav.Link>
-            <Nav.Link href="#about-me" className="nav-link">About me</Nav.Link>
-            <Nav.Link href="https://deinelieblingsjasmin.com" target="_blank" className="nav-link">My blogs</Nav.Link>
-            <NavDropdown title="🇺🇸" id="basic-nav-dropdown" menuVariant="dark">
-              <NavDropdown.Item href="/de.html" className="dropdown-item">🇩🇪 Deutsch</NavDropdown.Item>
-              <NavDropdown.Item href="/fr.html" className="dropdown-item">🇫🇷 Français</NavDropdown.Item>
-              <NavDropdown.Item href="/sc.html" className="dropdown-item">🇨🇳 简体中文</NavDropdown.Item>
-              <NavDropdown.Item href="/tc.html" className="dropdown-item">🇭🇰 繁體中文</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar> */}
     </>
   )
 }
