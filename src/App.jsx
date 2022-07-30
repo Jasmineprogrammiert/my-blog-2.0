5592e266b0e7b73e70010f251f0c92fe619b5ba8
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2022-07-30 12:01:58 +0800

    Create: my-blog-2.0

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavigationBar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/global.css';
+
+function App() {
+  return (
+    <>
+    <NavigationBar />
+    </>
+  );
+}
+
+export default App;
