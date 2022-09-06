80d17ed227f164106f54b4685463d2927b8016eb
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2022-09-06 09:17:21 +0800

    Create: useImgSlider

--- client/src/components/Blog/ImgSliderr.jsx
+++ client/src/components/Blog/ImgSliderr.jsx
@@ -1,46 +1,14 @@
 // hooks
 import usePath from '../../hooks/usePath';
 import useFetch from '../../hooks/useFetch';
+import useImgSlider from '../../hooks/useImgSlider';
 // styles
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Slider from 'react-slick';
 
 const ImgSliderr = () => {
   const { path } = usePath();
   const { data: blog } = useFetch('/blogs/' + path);
-
-  const NextArrow = ({ onClick }) => {
-    return (
-      <div className="img-arrow img-arrow-next" onClick={onClick}>
-        <ArrowForwardIosIcon />
-      </div>
-    );
-  };
-  const PrevArrow = ({ onClick }) => {
-    return (
-      <div className="img-arrow img-arrow-prev" onClick={onClick}>
-        <ArrowBackIosIcon />
-      </div>
-    );
-  };
-
-  const settings = {
-    lazyLoad: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    dotsClass: 'img_dots',
-    arrows: true,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    pauseOnHover: true,
-    fade: true
-  };
+  const { settings } = useImgSlider();
 
   return (
     <>
---
+++ client/src/hooks/useImgSlider.jsx
@@ -0,0 +1,40 @@
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+const useImgSlider = () => {
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="img-arrow img-arrow-next" onClick={onClick}>
+        <ArrowForwardIosIcon />
+      </div>
+    );
+  };
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="img-arrow img-arrow-prev" onClick={onClick}>
+        <ArrowBackIosIcon />
+      </div>
+    );
+  };
+
+  const settings = {
+    lazyLoad: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2800,
+    dots: true,
+    dotsClass: 'img_dots',
+    arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    pauseOnHover: true,
+    fade: true
+  };
+
+  return { settings }
+}
+
+export default useImgSlider;
