603070302ccfa285e793ec2ea12f910127161387
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2023-01-01 14:00:27 +0800

    On Progress: UpdateUsername.jsx

--- client/src/components/Settings/UpdateUsername.jsx
+++ client/src/components/Settings/UpdateUsername.jsx
@@ -1,6 +1,9 @@
 import { useState, useContext } from 'react';
 import axios from 'axios';
+// hooks and contexts
 import { AuthContext } from "../../context/AuthContext";
+// styles
+import bgImg from '../../assets/img/Settings/floral-1.jpg';
 
 const UpdateUsername = () => {
   const [username, setUsername] = useState('');
@@ -26,26 +29,58 @@ const UpdateUsername = () => {
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="username" 
-        name="username"
-        placeholder={user.username}
-        pattern="[A-Za-z0-9-_.]{4,25}"
-        title="The usernaem must be 4-25 long, with letters, numbers, hyphens, underscores or periods only. No punctuation or special characters are allowed."
-        required
-        onChange={e => setUsername(e.target.value)}
-      />
+    <div className="login">
+      <img src={bgImg} alt="Background" />
+      <div className="login-content">
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="username" 
+            name="username"
+            placeholder={user.username}
+            pattern="[A-Za-z0-9-_.]{4,25}"
+            title="The usernaem must be 4-25 long, with letters, numbers, hyphens, underscores or periods only. No punctuation or special characters are allowed."
+            required
+            onChange={e => setUsername(e.target.value)}
+          />
 
-      <button className="submit-btn" type="submit">
-        Update
-      </button>
-      {success && (
-        <span style={{ color: "green", textAlign: "center", marginTop: "20px" }}>
-          Profile has been updated...
-        </span>
-      )}
-    </form>
+          <button className="submit-btn" type="submit">
+            Update
+          </button>
+          {success && (
+            <span style={{ color: "green", textAlign: "center", marginTop: "20px" }}>
+              Profile has been updated...
+            </span>
+          )}
+        </form>
+      </div>
+    </div>
+
+    {/* <div className="login">
+      <img src={bgImg} alt="Background" />
+      <div className="login-content">
+        <h1>Lieblingsjasmin</h1>
+        <h2>Update</h2>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="username" 
+            name="username"
+            placeholder={user.username}
+            pattern="[A-Za-z0-9-_.]{4,25}"
+            title="The usernaem must be 4-25 long, with letters, numbers, hyphens, underscores or periods only. No punctuation or special characters are allowed."
+            required
+            onChange={e => setUsername(e.target.value)}
+          />
+          <button className="submit-btn" type="submit">
+            Update
+          </button>
+          {success && (
+            <span style={{ color: "green", textAlign: "center", marginTop: "20px" }}>
+             Profile has been updated...
+           </span>
+          )}
+        </form>
+      </div>
+    </div> */}
     </>
   )
 }
